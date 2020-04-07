2bbf60dfbc87624aa14f004f04fbecf028ba21e6
Luiz Felipe <lzfelipevs@gmail.com>
2020-04-07 20:59:39 -0300

    Updates

---
+++ precache-manifest.0ae1132e4c4fd2c4d50e4842cdbb3854.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2589d48bba3af720d05e00293ca0e6e",
+    "url": "/fveiga/index.html"
+  },
+  {
+    "revision": "e666627f7e0585a3d58d",
+    "url": "/fveiga/static/js/2.aacc4cf3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/fveiga/static/js/2.aacc4cf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31e017b139b2959d3518",
+    "url": "/fveiga/static/js/main.55d1aea4.chunk.js"
+  },
+  {
+    "revision": "e417861c25e17938ca05",
+    "url": "/fveiga/static/js/runtime-main.a04a4230.js"
+  },
+  {
+    "revision": "30ead64f7a54e10fa5208787e2cd7624",
+    "url": "/fveiga/static/media/behance.30ead64f.svg"
+  },
+  {
+    "revision": "c4b29dd2f7e5a0252abad259a229139e",
+    "url": "/fveiga/static/media/ig.c4b29dd2.svg"
+  },
+  {
+    "revision": "56a42ddf53d307de442abfd890dcb826",
+    "url": "/fveiga/static/media/linkedin.56a42ddf.svg"
+  },
+  {
+    "revision": "58592368f7f1fd1ccd06c4233cc46db1",
+    "url": "/fveiga/static/media/logo.58592368.svg"
+  },
+  {
+    "revision": "efe3b944d877601cdc05940ca21ecfaa",
+    "url": "/fveiga/static/media/logo_horz.efe3b944.svg"
+  }
+]);
